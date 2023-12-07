a4f2188bc7d71a5bc119231292e805b9d06279a0
chaerim <cofl0428428@naver.com>
2023-12-07 11:25:33 +0900

    first commit

---
+++ Project_03/src/com/javalec/base/Main.java
@@ -0,0 +1,17 @@
+package com.javalec.base;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		 * 1. Addition : 덧셈 - 수진
+		   2. AddSub : 덧셈 + 뺄셈 - 광영
+           3. AddSubMul : 덧셈+뺄셈+곱셈 - 수민
+           4. AddSubMulDiv : 덧셈+뺄셈+곱셈+나눗셈 - 채림
+           5. AddSubMulDivRemainder : 덧셈+뺄셈+곱셈+나눗셈+나머지 - 대근
+           6. Main?
+		 */
+
+	}
+
+}
