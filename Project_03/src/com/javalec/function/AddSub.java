9fed48ab04981b997f84e553b8b35cea46d5affa
Gwangyeong Kim <rhkddud0310@naver.com>
2023-12-07 11:59:10 +0900

    first commit

---
+++ Project_03/src/com/javalec/function/AddSub.java
@@ -0,0 +1,24 @@
+package com.javalec.function;
+
+public class AddSub extends Addition {
+	// Field
+	int sub = 0;
+	int firstNum, secondNum;
+	
+	// Constructor
+	public AddSub() {
+		this.sub = firstNum - secondNum;
+	}
+	
+	// Method
+	if(num1 > num2) {
+		firstNum = num1;
+		secondNum = num2;
+	}else {
+		firstNum = num2;
+		secondNum = num1;
+	}
+	
+	
+
+}
