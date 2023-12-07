51bd42fc6a83ea726594833fc343c0b3239c9eb5
ssujjy <ssujjy@gmail.com>
2023-12-07 11:47:46 +0900

    rename

---
+++ Project_03/src/com/javalec/function/Addition.java
@@ -0,0 +1,16 @@
+package com.javalec.function;
+
+public class Addition {
+	// Field
+	public int sum = 0;
+	public int num1 = 0;
+	public int num2 = 0;
+	// Constructor
+	public Addition(){
+		this.sum = num1 + num2;
+	}
+	
+	// Method 
+	
+	
+}
