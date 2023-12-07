3e3ab539297a81fc6fba123edf36c40ad3c97f6b
ssujjy <ssujjy@gmail.com>
2023-12-07 11:45:06 +0900

    first commit

---
+++ Project_03/src/com/javalec/function/Addtion.java
@@ -0,0 +1,16 @@
+package com.javalec.function;
+
+public class Addtion {
+	// Field
+	public int sum = 0;
+	public int num1 = 0;
+	public int num2 = 0;
+	// Constructor
+	public Addtion(){
+		this.sum = num1 + num2;
+	}
+	
+	// Method 
+	
+	
+}
