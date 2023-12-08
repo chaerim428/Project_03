cc63d037f29ef042ddb52a7c37a8667ef9d9d20a
ssujjy <ssujjy@gmail.com>
2023-12-08 10:20:53 +0900

    second commit

--- Project_03/src/com/javalec/function/Addition.java
+++ Project_03/src/com/javalec/function/Addition.java
@@ -2,15 +2,25 @@ package com.javalec.function;
 
 public class Addition {
 	// Field
-	public int sum = 0;
-	public int num1 = 0;
-	public int num2 = 0;
+	public int num1 = 0, num2 = 0, num3 = 0, num4 = 0;
+	
 	// Constructor
 	public Addition(){
-		this.sum = num1 + num2;
+		
 	}
-	
+//	
+//	
+//	public Addition(int num1, int num2) {
+//		sum2();
+//	}
+
+
 	// Method 
-	
+	public int sum2() {	// 2개 숫자 더하기.
+		return num1 + num2;
+	}
+	public int sum3() {	// 3개 숫자 더하기.
+		return num1 + num3 + num4;
+	}
 	
 }
