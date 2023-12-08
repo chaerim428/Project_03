6304c833922da6424e67d9a08d9106d21cd556e2
Sumin Kim <411sumin@gmail.com>
2023-12-08 12:16:36 +0900

    Final commit

---
+++ Project_03/src/com/javalec/function/AddSubMul.java
@@ -0,0 +1,57 @@
+package com.javalec.function;
+
+public class AddSubMul extends AddSub{
+
+		
+	// ---------------------------------------------------------------------------------------//
+	// Desc : Multiplying user input variables from Main and send back the resultant values. 
+	// Date : 2023.12.07(V.1)
+	// Author : Sumin Kim
+	// --------------------------------------------------------------------------------------//
+
+	
+	//Field
+	public AddSubMul() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	//Constructor
+	public void mulAction() {
+		System.out.println(num1*num3*num4);
+	}
+
+
+	//Method
+	public int mul1Action() {
+		return num1*num3*num4;
+	}
+	public int  mul2Action() {
+		return num3*num4;
+	}
+	
+	public int  mul3Action() {
+		return num1*num2*num3;
+	}
+	
+	public int mul4Action() {
+		return num1*num2*num4;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
